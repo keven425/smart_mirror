510a2f84eedbb763c580b3c92d2f9331eba7bd2c
Keven Wang <keven425@gmail.com>
2017-05-29 17:05:04 -0700

    handle keyboard event outside of loop

--- cv/tracking.py
+++ cv/tracking.py
@@ -45,88 +45,84 @@ class Tracking(object):
         if not self._detect_face and self._detect_smile:
             raise "in order to detect smile, must detect face"
 
-        try:
-            _return = {}
-            _return['face'] = False
-            _return['smile'] = False
+        _return = {}
+        _return['face'] = False
+        _return['smile'] = False
 
-            # Capture frame-by-frame
-            if environment.is_mac():
-                _, frame = self.video_capture.read()
-            else: # raspberry pi
-                frame = self.video_stream.read()
+        # Capture frame-by-frame
+        if environment.is_mac():
+            _, frame = self.video_capture.read()
+        else: # raspberry pi
+            frame = self.video_stream.read()
 
-            if frame is None:
-                return _return # camera might not be ready yet
+        if frame is None:
+            return _return # camera might not be ready yet
 
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            # hands = self.hand_cascade.detectMultiScale(
-            #     gray,
-            #     scaleFactor=1.3,
-            #     minNeighbors=5,
-            #     minSize=(30, 30),
-            #     flags=cv2.CASCADE_SCALE_IMAGE
-            # )
-            #
-            # # Draw a rectangle around the faces
-            # for (x, y, w, h) in hands:
-            #     cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        # hands = self.hand_cascade.detectMultiScale(
+        #     gray,
+        #     scaleFactor=1.3,
+        #     minNeighbors=5,
+        #     minSize=(30, 30),
+        #     flags=cv2.CASCADE_SCALE_IMAGE
+        # )
+        #
+        # # Draw a rectangle around the faces
+        # for (x, y, w, h) in hands:
+        #     cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-            if self._detect_face:
-                faces = self.face_cascade.detectMultiScale(
-                    gray,
-                    scaleFactor=1.3,
-                    minNeighbors=5,
-                    minSize=(30, 30),
+        if self._detect_face:
+            faces = self.face_cascade.detectMultiScale(
+                gray,
+                scaleFactor=1.3,
+                minNeighbors=5,
+                minSize=(30, 30),
+                flags=cv2.CASCADE_SCALE_IMAGE
+            )
+
+            # Draw a rectangle around the faces
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+            # if face detected
+            face_detected = hasattr(faces, 'shape')
+            _return['face'] = face_detected
+
+            if face_detected and self._detect_smile:
+                # detect smile
+                # pick largest face
+                faces_area = faces[:, 2] * faces[:, 3]
+                face = faces[np.argmax(faces_area)]
+
+                # find smile within face
+                x, y, w, h = face
+                roi_gray = gray[y:y + h, x:x + w]
+                roi_color = frame[y:y + h, x:x + w]
+
+                smiles = self.smile_cascade.detectMultiScale(
+                    roi_gray,
+                    scaleFactor=1.7,
+                    minNeighbors=22,
+                    minSize=(25, 25),
                     flags=cv2.CASCADE_SCALE_IMAGE
                 )
 
-                # Draw a rectangle around the faces
-                for (x, y, w, h) in faces:
-                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                # Set region of interest for smiles
+                for (x, y, w, h) in smiles:
+                    cv2.rectangle(roi_color, (x, y), (x + w, y + h), (0, 0, 255), 3)
 
-                # if face detected
-                face_detected = hasattr(faces, 'shape')
-                _return['face'] = face_detected
+                # if smile detected
+                _return['smile'] = hasattr(smiles, 'shape')
 
-                if face_detected and self._detect_smile:
-                    # detect smile
-                    # pick largest face
-                    faces_area = faces[:, 2] * faces[:, 3]
-                    face = faces[np.argmax(faces_area)]
+        # Display the resulting frame
+        cv2.imshow('Video', frame)
 
-                    # find smile within face
-                    x, y, w, h = face
-                    roi_gray = gray[y:y + h, x:x + w]
-                    roi_color = frame[y:y + h, x:x + w]
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            exit(0)
 
-                    smiles = self.smile_cascade.detectMultiScale(
-                        roi_gray,
-                        scaleFactor=1.7,
-                        minNeighbors=22,
-                        minSize=(25, 25),
-                        flags=cv2.CASCADE_SCALE_IMAGE
-                    )
+        return _return
 
-                    # Set region of interest for smiles
-                    for (x, y, w, h) in smiles:
-                        cv2.rectangle(roi_color, (x, y), (x + w, y + h), (0, 0, 255), 3)
-
-                    # if smile detected
-                    _return['smile'] = hasattr(smiles, 'shape')
-
-            # Display the resulting frame
-            cv2.imshow('Video', frame)
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                exit(0)
-
-            return _return
-        except KeyboardInterrupt:
-            self._cleanup()
-            sys.exit(0)
-    
     def _cleanup(self):
         print('cleaning up')
         # When everything is done, release the capture
@@ -140,6 +136,10 @@ if __name__ == '__main__':
     tracking = Tracking()
     tracking.detect_face(True)
     tracking.detect_smile(True)
-    while True:
-        tracking.detect()
+    try:
+        while True:
+            tracking.detect()
+    except KeyboardInterrupt:
+        tracking._cleanup()
+        sys.exit(0)
 
